8c53d0bd38c8accdc2230534a1f5e3f75b1f1216
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-20 23:50:34 +0300

    preparation for sending request and handling response

--- CashaWeb/casha-react-application/src/Login.js
+++ CashaWeb/casha-react-application/src/Login.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from './context/AuthProvider';
+import axios from './api/axios';
+const LOGIN_URL = '/address from back for this page';
 
 function Login() {
+    const { setAuth } = useContext(AuthContext);
+
     const userRef = useRef();
     const errRef = useRef();
 
@@ -14,13 +19,48 @@ function Login() {
         setErrMsg('');
     }, [user, pwd])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ userName: user, password: pwd }),
+                {
+                    headers: { 'Content-type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            //Names same as back expectes
+            console.log(JSON.stringify(response?.data)); //temp
+            const accessToken = response?.data?.accessToken;  //what back returns
+            const roles = response?.data?.roles; //what back returns
+            setAuth({user,pwd,roles,accessToken}); //storing information in this object and store it in global context
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+
+        } catch (err) {                 //errors that expected from back
+            if(!err?.response){
+                alert("No Server Response");
+            } else if(err.response?.status === 400){
+                alert("Missing Password or Login");
+            } else if(err.response?.status === 401){
+                alert("Unathorized")
+            }else{
+                alert("Login failed");
+            }
+
+        }
+
+        //console.log(JSON.stringify({user, pwd}));
+    }
+
     return (
         <div class="Login_Wrapper">
             <section>
                 <div class="Login_Image_Container">
                     <img src='./Img/Logo.png'></img>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="Form_Input_Area_Wrapper">
                         <div class="Login_Input_Wrap">
                             <div class="Input_Blocks First_Input_Block">
@@ -34,7 +74,9 @@ function Login() {
                                     <input
                                         type="text"
                                         id="username"
+                                        ref={userRef}
                                         autoComplete="off"
+                                        onChange={(e) => setUser(e.target.value)}
                                         required
                                     />
                                 </div>
@@ -51,16 +93,16 @@ function Login() {
                                     <input
                                         type="password"
                                         id="password"
+                                        onChange={(e) => setPwd(e.target.value)}
+                                        value={pwd}
                                         required
                                     />
                                 </div>
-
                             </div>
-
                         </div>
                     </div>
                     <div class="Button_Wrapper">
-                            <button>Log In</button>
+                        <button>Log In</button>
 
                     </div>
 
