98af9b87572034186cd1cbf560a4981068a1ff01
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-21 21:13:17 +0300

    Add elements to header component

--- CashaWeb/casha-react-application/src/components/adminHeaderCompoment/AdminHeader.jsx
+++ CashaWeb/casha-react-application/src/components/adminHeaderCompoment/AdminHeader.jsx
@@ -7,18 +7,23 @@ function adminHeader() {
         <header>
             <div className="wrapper">
                 <div className="logo">
-                    <img src="" alt="Bad Response" />
+                    <img src="./img/HeaderLogo.png" alt="Bad Response" />
                     <a href=''></a>
                 </div>
                 <div className="search">
                     <form>
-                        <input type={'text'}></input>
-                        
-                        <button type="submit"></button>
+                        <div class="form_wrapper">
+                            <div class="form_input">
+                                <input type={'text'} placeholder={'Type for search'} />
+                            </div>
+                                <button class = "submit_button" type="submit">
+                                    <img src="./img/SearchIcon.png" alt="Nothing to serch" />
+                                </button>
+                        </div>
                     </form>
                 </div>
                 <div className="avatar">
-                    <img src="" alt="Bad response" />
+                    <img src="./img/AvatarIcon.png" alt="Bad response" />
                 </div>
             </div>
         </header>
