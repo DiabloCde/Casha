820bcee80de35a549ce59dee4a57a4abd96f393c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-10-22 22:41:40 +0300

    Impl ILoginService

---
+++ CashaWeb/Casha.BLL/Services/LoginService.cs
@@ -0,0 +1,30 @@
+﻿using Casha.BLL.Interfaces;
+using Casha.DAL.Interfaces;
+
+namespace Casha.BLL.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenService _tokenService;
+
+        public LoginService(IUserRepository userRepository, ITokenService tokenService)
+        {
+            _userRepository = userRepository;
+            _tokenService = tokenService;
+        }
+
+        public string Login(string username, string password)
+        {
+            // TODO: decifer password before comparison
+            var logged = _userRepository.GetUsers(u => u.UserName == username && u.PasswordHash == password).First();
+
+            if (logged == null)
+            {
+                throw new InvalidOperationException("User not found");
+            }
+
+            return _tokenService.GenerateToken(logged);
+        }
+    }
+}
