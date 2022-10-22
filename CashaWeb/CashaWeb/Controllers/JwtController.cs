474dd149eb524e3bce9d24187d50e4f319fd68f0
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-10-22 20:31:59 +0300

    JwtController add template code

---
+++ CashaWeb/CashaWeb/Controllers/JwtController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CashaWeb.Controllers
+{
+    [Route("api/security/[controller]")]
+    [ApiController]
+    public class JwtController : Controller
+    {
+        private IConfiguration _config;
+
+        public JwtController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> VerifyEmploee(Dictionary<string, string> credentials)
+        {
+            string issuer = _config["Jwt:Issuer"];
+            string audience = _config["Jwt:Audience"];
+            byte[] key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+
+            SecurityTokenDescriptor tokenDescriptor;
+            try
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim("Id", Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub, credentials["Login"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Role, "Admin")
+                };
+
+                tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials
+                    (new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha512Signature)
+                };
+            }
+            catch (NullReferenceException ex)
+            {
+                return BadRequest(ex.Message + " Emploee not found.");
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return Ok(stringToken);
+        }
+    }
+}
