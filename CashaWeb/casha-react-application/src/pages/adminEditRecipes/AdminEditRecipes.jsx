678a77f54e2bb7daa7428cc4a31ef0541f7bdd2e
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-22 07:35:45 +0300

    add something to content of page

--- CashaWeb/casha-react-application/src/pages/adminEditRecipes/AdminEditRecipes.jsx
+++ CashaWeb/casha-react-application/src/pages/adminEditRecipes/AdminEditRecipes.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React from 'react';
+
 import AdminHeader from '../../components/adminHeaderCompoment/AdminHeader';
+import AdminNavbar from '../../components/adminNavbarComponent/AdminNavbar';
+
+import styles from './AdminEditRecipes.css'
 
 function AdminEditRecipes() {
     return (
-        <body>
-            <AdminHeader>
+        <>
+            <AdminHeader />
+            <AdminNavbar />
+            <main class='main'>
+               
+                <div className="form_block">
+                    <h1>Recipes</h1>
+                    <form action="POST" className="form">
+                        <div className="input_block">
+                            <label htmlFor="name">Name:</label>
+                            <input id="name" type="text" />
+                        </div>
+                        <div className="input_block">
+                            <label htmlFor="cuisine">Cuisine:</label>
+                            <input type="text" id="cuisine" />
+                        </div>
+                        <div className="input_block">
+                            <label htmlFor="holidays">Holidays:</label>
+                            <input type="text" id="holidays" />
+                        </div>
+                        <div className="input_block">
+                            <label htmlFor="complexity">Complexity:</label>
+                            <input type="text" id="complexity" />
+                        </div>
+                    </form>
+                </div>
+                <div className="scrolling">
+                    
+                    </div>
+            </main>
 
-            </AdminHeader>
-            <div>
-
-            </div>
-        </body>
+        </>
     )
 }
 
