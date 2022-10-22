2bbe6dcd0750af51f415dba4dd3e886d085e7861
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-10-22 21:55:00 +0300

    Create ITokenService interface

---
+++ CashaWeb/Casha.BLL/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using Casha.Core.DbModels;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Casha.BLL.Interfaces
+{
+    public interface ITokenService
+    {
+        public SecurityTokenDescriptor GenerateToken(User user);
+    }
+}
