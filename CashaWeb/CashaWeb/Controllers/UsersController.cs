de757e9db9258f28237cc28ff446412c24852f34
Vlad Bocharov <vladislavboc11@gmail.com>
2022-10-24 17:58:42 +0300

    Added GetUser API request

---
+++ CashaWeb/CashaWeb/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Casha.DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Casha.DAL;
+using Casha.Core.DbModels;
+using System.Linq.Expressions;
+
+namespace CashaWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser(int id)
+        {
+            User? user = _userRepository.GetUserByID(id.ToString());
+
+            if(user != null)
+            {
+                return Ok(user);
+            }
+
+            return BadRequest();
+        }
+    }
+}
