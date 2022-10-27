0b5cd4d2bfa965fcbcafb1e5f85027dc952edfc6
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-27 18:43:30 +0300

    Change form displaying

--- CashaWeb/casha-react-application/src/components/editRecipesBlockComponent/EditRecipesBlock.jsx
+++ CashaWeb/casha-react-application/src/components/editRecipesBlockComponent/EditRecipesBlock.jsx
@@ -3,34 +3,55 @@ import React from 'react'
 import styles from './EditRecipesBlock.css';
 
 function EditRecipesBlock() {
-    const sendRecipe = () =>{
-        document.getElementById('editRecipes_block').style.visibility ='hidden';
+    const sendRecipe = () => {
+        document.getElementById('editRecipes_block').style.display = 'none';
+        document.getElementById('form_block').style.display = 'block';
     }
 
     return (
-        <div className='editRecipes_block' id = 'editRecipes_block' >
+        <div className='editRecipes_block' id='editRecipes_block' >
             <h1>Add/Edit recipe</h1>
             <form action="POST">
-                <label htmlFor="name">Name:</label>
-                <input type="text" id ="name"/>
-                <label htmlFor="cuisine">Type of cuisine:</label>
-                <input type="text" id ="cuisine"/>
-                <label htmlFor="holidays">Holidays:</label>
-                <input type="text" id ="holidays" />
-                <label htmlFor="complexity">Complexity:</label>
-                <input type="radio" name="complexity" id="complexityEasy" value={"Easy"}/>
+                <div className="input_block">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" />
+                </div>
+                <div className="input_block">
+                    <label htmlFor="cuisine">Type of cuisine:</label>
+                    <input type="text" id="cuisine" />
+                </div>
+                <div className="input_block">
+                    <label htmlFor="holidays">Holidays:</label>
+                    <input type="text" id="holidays" />
+                </div>
+                <div className="input_block">
+                    <label htmlFor="complexity">Complexity:</label>
+                    <input type="radio" name="complexity" id="complexityEasy" value={"Easy"} />
+                </div>
+                <div className="input_block">
                 <label htmlFor="complexityEasy">Easy</label>
                 <input type="radio" name="complexity" id="complexityMedium" value={"Medium"} />
+                </div>
+                <div className="input_block">
                 <label htmlFor="complexityMedium">Medium</label>
                 <input type="radio" name="complexity" id="complexityHard" value={"Hard"} />
+                </div>
+                <div className="input_block">
                 <label htmlFor="complexityHard">Hard</label>
                 <label htmlFor="ingredients">Ingredients</label>
+                </div>
+                <div className="input_block">
                 <input type="text" id="ingredients" />
+                </div>
+                <div className="input_block">
                 <label htmlFor="image">Image:</label>
                 <input type="image" id="image" src="" alt="" />
+                </div>
+                <div className="input_block">
                 <label htmlFor="instructions">Instructions</label>
-                <input type="text" id='instructions'/>
-                <button type="submit" onClick={sendRecipe}></button>
+                <input type="text" id='instructions' />
+                </div>
+                <button className='submit_button_addrecipe' type="submit" onClick={sendRecipe}>Submit</button>
 
             </form>
         </div>
