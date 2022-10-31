c49687f55e674673b5e909362f6b248763340dd9
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-31 09:18:33 +0300

    Create IPostRepository with methods

---
+++ CashaWeb/Casha.DAL/Interfaces/IPostRepository.cs
@@ -0,0 +1,24 @@
+﻿using Casha.Core.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casha.DAL.Interfaces
+{
+    internal interface IPostRepository
+    {
+        List<Post> GetPosts(Expression<Func<Post, bool>> filter);
+
+        Post? GetPostByID(string postId);
+
+        void CreatePost(Post post);
+
+        void DeletePost(string postId);
+
+        void UpdatePost(Post post);
+
+    }
+}
