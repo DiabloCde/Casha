f56067b5ec9fdb1b4f5b0513d4167561dd7de5ff
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-31 09:20:09 +0300

    Add default methods from interface

--- CashaWeb/Casha.DAL/Repositories/PostRepository.cs
+++ CashaWeb/Casha.DAL/Repositories/PostRepository.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Casha.Core.DbModels;
+using Casha.DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Casha.DAL.Repositories
 {
-    internal class PostRepository
+    internal class PostRepository : IPostRepository
     {
+        public void CreatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePost(string postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post? GetPostByID(string postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetPosts(Expression<Func<Post, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
