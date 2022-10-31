1c8c1d96028a675319fcd10cbccecdedcb586177
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-31 10:51:48 +0300

    Add properties to PostViewModel

--- CashaWeb/CashaWeb/ViewModels/PostViewModel.cs
+++ CashaWeb/CashaWeb/ViewModels/PostViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace CashaWeb.ViewModels
+﻿using Casha.Core.DbModels;
+
+namespace CashaWeb.ViewModels
 {
     public class PostViewModel
     {
+        public int PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime PostedDate { get; set; }
+
+        public string Description { get; set; }
+
+        public string UserId { get; set; }
     }
 }
