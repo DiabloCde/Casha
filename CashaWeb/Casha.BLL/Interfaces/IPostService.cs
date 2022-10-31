53bb16f013aba8d138407b962e70f32ead2038c2
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-31 10:23:33 +0300

    Create IPostService

---
+++ CashaWeb/Casha.BLL/Interfaces/IPostService.cs
@@ -0,0 +1,31 @@
+﻿using Casha.Core.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casha.BLL.Interfaces
+{
+    internal interface IPostService
+    {
+        Post? GEtPostById(int postId);
+
+        List<Post> GetAllPosts();
+
+        List<Post> GetPostsFiltered(string userId = "");
+
+        void CreatePost(Post post);
+        void UpdatePost(Post post);
+        void DeltePost(int psotId);
+
+        /*void UpdateUser(User user);
+
+        void DeleteUser(string userId);
+
+        User? GetUserDetails(string userId);
+
+        List<User> GetUsers(bool isAllUsers = true, bool isCertifiedUsers = false, string search = "");
+        */
+    }
+}
