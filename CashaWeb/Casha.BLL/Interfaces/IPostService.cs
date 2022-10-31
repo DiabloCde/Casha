2f784cfe920911de4bd62a93e912d70d56ee26c9
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-31 11:01:53 +0300

    Formatting IPostServcie

--- CashaWeb/Casha.BLL/Interfaces/IPostService.cs
+++ CashaWeb/Casha.BLL/Interfaces/IPostService.cs
@@ -16,16 +16,9 @@ namespace Casha.BLL.Interfaces
         List<Post> GetPostsFiltered(string userId = "");
 
         void CreatePost(Post post);
+
         void UpdatePost(Post post);
+
         void DeltePost(int psotId);
-
-        /*void UpdateUser(User user);
-
-        void DeleteUser(string userId);
-
-        User? GetUserDetails(string userId);
-
-        List<User> GetUsers(bool isAllUsers = true, bool isCertifiedUsers = false, string search = "");
-        */
     }
 }
