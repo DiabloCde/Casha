a0da2dbdef076b29dc3088e53b8686be79400145
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-01 18:44:28 +0300

    Create PostCreateModel

---
+++ CashaWeb/CashaWeb/ViewModels/PostCreateModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CashaWeb.ViewModels
+{
+    public class PostCreateModel
+    {
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public DateTime? PostedDate { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+
+    }
+}
