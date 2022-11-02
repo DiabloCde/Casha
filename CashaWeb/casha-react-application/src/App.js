a399f907713420aa2b5b27038cc8c0efb1d2d59b
MariaLiashenko <76902466+MariaLiashenko@users.noreply.github.com>
2022-11-02 14:58:43 +0200

    OtherUserMainPage
    component and fetch

--- CashaWeb/casha-react-application/src/App.js
+++ CashaWeb/casha-react-application/src/App.js
@@ -2,6 +2,7 @@ import Login from "./pages/loginPage/Login.jsx";
 import Register from "./pages/registerPage/Register.jsx";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminEditRecipes from "./pages/adminEditRecipes/AdminEditRecipes.jsx";
+import OtherUserMainPage from "./pages/otherUserMainPage/otherUserMainPage.jsx";
 
 function App() {
     return (
@@ -10,6 +11,8 @@ function App() {
                 <Route path="/Login" element={<Login />} />
                 <Route path="/Register" element={<Register />} />
                 <Route path="/EditRecipes" element={<AdminEditRecipes />} />
+                <Route path="/OtherUserMainPage" element={<OtherUserMainPage />} />
+
                 {/* Default Router */}
                 <Route path="/" element={<Navigate to="/Login" />} />
             </Routes>
--- CashaWeb/casha-react-application/src/pages/otherUserMainPage/otherUserMainPage.jsx
+++ CashaWeb/casha-react-application/src/pages/otherUserMainPage/otherUserMainPage.jsx
@@ -1,11 +1,48 @@
 import React from "react"
-// import styled from "styled-components"
 import CreateProfileBlog from "../../components/createProfileBlogComponent/createProfileBlog"
 import UserHeader from "../../components/userHeaderComponent/UserHeader"
+import ProfileMenu from "../../components/ProfileMenuComponent/ProfileMenu"
+import { useRef, useState, useEffect, useContext } from "react"
+import { Routes, Route, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+import axios from "axios"
+
+const URL =
+  "https://localhost:7128/api/User/028f54b4-6c1e-4533-a81d-616a2e4c065b"
+
 function OtherUserMainPage() {
+  const profileMenuTexts = ["Overview", "User recipes"]
+  const profileMenuLinks = ["/to", "/to"]
+  const activeText = "Overview"
+  async function render() {
+    const response = await axios({
+      method: "get",
+      url: URL,
+      data: JSON.stringify(),
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+    }).then((response) => {
+      console.log(response.data)
+    })
+    return response.data
+  }
+
+  var object = render()
+  console.log(object.data)
+
+  // console.log()
   return (
     <>
       <UserHeader />
+      <ProfileMenu
+        profileMenuTexts={profileMenuTexts}
+        profileMenuLinks={profileMenuLinks}
+        activeText={activeText}
+        imgLink={
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/640px-American_Beaver.jpg"
+        }
+        userNickName={"popabebra"}
+      />
       <CreateProfileBlog />
     </>
   )
