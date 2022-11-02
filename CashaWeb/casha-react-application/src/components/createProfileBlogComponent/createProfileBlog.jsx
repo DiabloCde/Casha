a23a67f3bb7c35ecc2bff141db4c251ba687cb80
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-02 21:46:53 +0300

    Requests to back

--- CashaWeb/casha-react-application/src/components/createProfileBlogComponent/createProfileBlog.jsx
+++ CashaWeb/casha-react-application/src/components/createProfileBlogComponent/createProfileBlog.jsx
@@ -1,28 +1,66 @@
 import React from "react"
+import { useRef, useState, useEffect, useContext } from "react"
+import axios from "axios"
 
 import styles from "./createProfileBlog.css"
 
+const URL_GETUSERPOSTS = "https://localhost:7128/api/Post/UserPosts/e802fce3-10bc-4ce4-a90f-bc94967eee13"
+
 function CreateProfileBlog() {
-  return (
-    <div className="wrapperBlog">
-      <h1>Blog:</h1>
-      <div className="blogs">
-        <div className="postOfReceipt">
-          <div className="postInfo">
-            <img src="./img/ico.png" alt="" />
-            <p>nickname</p>
-            <p>12.02.2003</p>
-          </div>
-          <div className="postMainInfo">
-            <p>Название:</p>
-            <img src="./img/Pic.png" alt="" />
-            <button className="btn openReceipt">Open receipt</button>
-            <p>Описание</p>
-            <button className="btn">Comments</button>
-          </div>
+  const [userPosts, setUserPosts] = useState([])
+  const [isRendered, setIsRendered] = useState(false)
+
+
+
+  async function getUserPosts() {
+    const response = await axios({
+      method: "get",
+      url: URL_GETUSERPOSTS,
+      data: JSON.stringify(),
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+    }).then((response) => {
+      setUserPosts(response.data)
+    })
+  }
+
+  useEffect(() => {
+    getUserPosts();
+    setIsRendered(true);
+  }, [])
+
+  if (isRendered) {
+    console.log(userPosts)
+    return (
+      <div className="wrapperBlog">
+        <h1>Blog:</h1>
+        <div className="blogs">
+          {userPosts?.reverse().map((item) =>
+            <>
+              <div className="postOfReceipt">
+                <div className="postInfo">
+                  <img key={item.postId} src={item.profilePictureUrl} alt="" />
+                  <p key={item.postId}>{item.displayName}</p>
+                  <p key={item.postId}>{item.displayName}</p>
+                </div>
+                <div className="postMainInfo">
+                  <p key={item.postId}>{item.title}</p>
+                  <img src="./img/Pic.png" alt="" />
+                  <button className="btn openReceipt">Open receipt</button>
+                  <p key={item.postId}>{item.description}</p>
+                  <button className="btn">Comments</button>
+                </div>
+              </div>
+
+            </>)}
+
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+  else {
+    return (
+      <h1>Please Wait</h1>
+    )
+  }
 }
 export default CreateProfileBlog
--- CashaWeb/casha-react-application/src/pages/otherUserMainPage/otherUserMainPage.jsx
+++ CashaWeb/casha-react-application/src/pages/otherUserMainPage/otherUserMainPage.jsx
@@ -8,49 +8,54 @@ import { Link } from "react-router-dom"
 
 import axios from "axios"
 
-const URL =
+const URL_GETUSER =
   "https://localhost:7128/api/User/028f54b4-6c1e-4533-a81d-616a2e4c065b"
+
+const URL_GETUSERPOSTS = "https://localhost:7128/api/Post/UserPosts/e802fce3-10bc-4ce4-a90f-bc94967eee13"
 
 function OtherUserMainPage() {
 
   const [userInfo, setUserInfo] = useState({})
-  const [userPosts, setUserPosts] = useState({})
   const [isRendered, setIsRendered] = useState(false)
   const profileMenuTexts = ["Overview", "User recipes"]
   const profileMenuLinks = ["/to", "/to"]
   const activeText = "Overview"
 
-  useEffect(() => {
-    setUserInfo(getUser());
-  },[])
 
   async function getUser() {
     const response = await axios({
       method: "get",
-      url: URL,
+      url: URL_GETUSER,
       data: JSON.stringify(),
       headers: { "Content-Type": "application/json; charset=utf-8" },
     }).then((response) => {
       setUserInfo(response.data)
-      setIsRendered(true)
-      return response.data
     })
   }
 
 
+  useEffect(() => {
+    getUser();
+   setIsRendered(true);
+ }, [])
 
-  return (
-    <>
-      <UserHeader />
-      <ProfileMenu
-        profileMenuTexts={profileMenuTexts}
-        profileMenuLinks={profileMenuLinks}
-        activeText={activeText}
-        imgLink={userInfo.profilePictureUrl}
-        userNickName={userInfo.displayName}
-      />
-      <CreateProfileBlog />
-    </>
-  )
+  if (isRendered === true) {
+    console.log(userInfo);
+    return (
+      <>
+        <UserHeader />
+        <ProfileMenu
+          profileMenuTexts={profileMenuTexts}
+          profileMenuLinks={profileMenuLinks}
+          activeText={activeText}
+          imgLink={userInfo.profilePictureUrl}
+          userNickName={userInfo.displayName}
+        />
+        <CreateProfileBlog />
+      </>
+    )
+  } else {
+    return <h1>Wait please LOADING...</h1>
+  }
 }
 export default OtherUserMainPage
