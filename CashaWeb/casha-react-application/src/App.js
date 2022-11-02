56b46ab35c6d8cab1fbd002a06c087395b516b4e
Vlad Bocharov <vladislavboc11@gmail.com>
2022-11-02 16:04:14 +0200

    Undo App.js

--- CashaWeb/casha-react-application/src/App.js
+++ CashaWeb/casha-react-application/src/App.js
@@ -2,18 +2,16 @@ import Login from "./pages/loginPage/Login.jsx";
 import Register from "./pages/registerPage/Register.jsx";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminEditRecipes from "./pages/adminEditRecipes/AdminEditRecipes.jsx";
-import ProfileSettings from "./pages/ProfileSettings/ProfileSettings.js";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<ProfileSettings />
 			<Routes>
-				{/* <Route path="/Login" element={<Login />} />
+				<Route path="/Login" element={<Login />} />
 				<Route path="/Register" element={<Register />} />
-				<Route path="/EditRecipes" element={<AdminEditRecipes />} /> */}
+				<Route path="/EditRecipes" element={<AdminEditRecipes />} />
 				{/* Default Router */}
-				{/* <Route path="/" element={<Navigate to="/Login" />} /> */}
+				<Route path="/" element={<Navigate to="/Login" />} />
 			</Routes>
 		</BrowserRouter>
 	);
