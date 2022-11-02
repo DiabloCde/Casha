6c3d38b933dbac6b5e40b2e1eebc9373f4174637
Vlad Bocharov <vladislavboc11@gmail.com>
2022-11-02 16:07:11 +0200

    Added Router to ProfileSettings page

--- CashaWeb/casha-react-application/src/App.js
+++ CashaWeb/casha-react-application/src/App.js
@@ -2,6 +2,7 @@ import Login from "./pages/loginPage/Login.jsx";
 import Register from "./pages/registerPage/Register.jsx";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminEditRecipes from "./pages/adminEditRecipes/AdminEditRecipes.jsx";
+import ProfileSettings from "./pages/ProfileSettings/ProfileSettings.js";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Route path="/Login" element={<Login />} />
 				<Route path="/Register" element={<Register />} />
 				<Route path="/EditRecipes" element={<AdminEditRecipes />} />
+				<Route path="/ProfileSettings" element={<ProfileSettings />} />
 				{/* Default Router */}
 				<Route path="/" element={<Navigate to="/Login" />} />
 			</Routes>
