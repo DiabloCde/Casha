16e886bc7529ef71cf2701dc4e39a1c63219b7c8
Vlad Bocharov <vladislavboc11@gmail.com>
2022-11-02 13:56:10 +0200

    Added update user function

--- CashaWeb/casha-react-application/src/pages/ProfileSettings/ProfileSettings.js
+++ CashaWeb/casha-react-application/src/pages/ProfileSettings/ProfileSettings.js
@@ -14,6 +14,7 @@ const API_Imgur = "https://api.imgur.com/3/image/";
 const Client_ID = "e9a6b3bb9d8860f";
 
 const URL_USER = "https://localhost:7128/api/User/";
+const USER_ID = "028f54b4-6c1e-4533-a81d-616a2e4c065b";
 
 function ProfileSettings() {
 	const [user, setUser] = useState([]);
@@ -31,52 +32,74 @@ function ProfileSettings() {
 	// TODO onLoad function
 	useEffect(() => {
 		getUser();
-		setTempVariables();
 	}, []);
 
+	useEffect(() => {
+		setTempVariables();
+	}, [user]);
+
 	// TODO API GET user (user id in cookies)(set user data into variable 'user')
-	function getUser() {
-		// fetch(URL_USER + "028f54b4-6c1e-4533-a81d-616a2e4c065b", {
-		// 	method: "GET",
-		// 	headers: {
-		// 		Accept: "application/json",
-		// 		"Content-Type": "application/json"
-		// 	}
-		// })
-		// 	.then(res => res.json())
-		// 	.then(
-		// 		result => {
-		// 			console.log(result);
-		// 		},
-		// 		error => {
-		// 			alert("Failed");
-		// 		}
-		// 	);
-		const response = axios.get(URL_USER + "028f54b4-6c1e-4533-a81d-616a2e4c065b", JSON.stringify(), {
-			headers: { "Content-type": "application/json" },
-			withCredentials: true
-		});
+	async function getUser() {
+		try {
+			const response = await axios.get(URL_USER + USER_ID);
+			setUser(response.data);
+			//setTempVariables();
+		} catch (err) {
+			//errors that expected from back
+			if (!err?.response) {
+				alert("No Server Response");
+			} else if (err.response?.status === 400) {
+				alert("Missing Password or Login");
+			} else if (err.response?.status === 401) {
+				alert("Unathorized");
+			} else {
+				alert("Login failed");
+			}
+		}
 		//Names same as back expectes
-		console.log(JSON.stringify(response?.data)); //temp
-		console.log(123);
 	}
 
 	// TODO SET temporary variables to store user changes
 	function setTempVariables() {
+		console.log(user);
 		// temporary
 		// take data from user!!!
-		setUserImg(
-			"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1"
-		);
-		setUserNickName("User1");
-		setUserName();
-		setUserSurname();
-		setUserEmail();
-		setUserBio();
+		setUserImg(user.profilePictureUrl);
+		setUserNickName(user.displayName);
+		setUserName(user.firstName);
+		setUserSurname(user.lastName);
+		//setUserEmail();
+		setUserBio(user.bio);
 	}
 
 	// TODO API update user data (take data from temporary variables)
-	function updateUserData() {}
+	const handleSubmit = async e => {
+		e.preventDefault();
+		let newUser = {
+			id: user.id,
+			bio: userBio,
+			displayName: userNickName,
+			firstName: userName,
+			lastName: userSurname,
+			isCertified: user.isCertified,
+			profilePictureUrl: userImg
+		};
+		try {
+			const response = await axios.put(URL_USER + USER_ID, newUser);
+			console.log(response);
+		} catch (err) {
+			//errors that expected from back
+			if (!err?.response) {
+				alert("No Server Response");
+			} else if (err.response?.status === 400) {
+				alert("Missing Password or Login");
+			} else if (err.response?.status === 401) {
+				alert("Unathorized");
+			} else {
+				alert("Login failed");
+			}
+		}
+	};
 
 	// TODO API change user password (data: currentPassword, newPassword)
 	function changeUserPassword() {}
@@ -171,7 +194,8 @@ function ProfileSettings() {
 										/>
 									</InputGroup>
 								</div>
-								<div className="container row p-2">
+								{/* User Email */}
+								{/* <div className="container row p-2">
 									<h5>Change your email</h5>
 									<InputGroup className="mb-3">
 										<FormControl
@@ -183,7 +207,7 @@ function ProfileSettings() {
 											required
 										/>
 									</InputGroup>
-								</div>
+								</div> */}
 							</div>
 						</div>
 						<div className="row">
@@ -205,7 +229,7 @@ function ProfileSettings() {
 							<div className="row container">
 								<div className="d-flex justify-content-end p-2">
 									{/* // TODO add onClick function to edit user data */}
-									<Button className="btn btn-sm btn-warning yellowButton">
+									<Button onClick={handleSubmit} className="btn btn-sm btn-warning yellowButton">
 										<strong className="colorBlack">Save</strong>
 									</Button>
 								</div>
