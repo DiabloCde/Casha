181ca2e2405119c8526698e585dccfedf5c86049
ulyasaur <ulyano4ka2003@gmail.com>
2022-11-19 22:12:14 +0200

    created RecipeService

---
+++ CashaWeb/Casha.BLL/Interfaces/IRecipeService.cs
@@ -0,0 +1,24 @@
+﻿using Casha.Core.DbModels;
+using Casha.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casha.BLL.Interfaces
+{
+    public interface IRecipeService
+    {
+        List<Recipe> GetRecipes(RecipeFilterDto recipeFilter);
+
+        Recipe? GetRecipeByID(int recipeId);
+
+        void AddRecipe(Recipe recipe);
+
+        void DeleteRecipe(int recipeId);
+
+        void UpdateRecipe(Recipe recipe);
+    }
+}
---
+++ CashaWeb/Casha.Core/Dtos/RecipeFilterDto.cs
@@ -0,0 +1,23 @@
+﻿using Casha.Core.DbModels;
+using Casha.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casha.Core.Dtos
+{
+    public class RecipeFilterDto
+    {
+        public string Name { get; set; } = "";
+
+        public Difficulty? Difficulty { get; set; } = null;
+
+        public string UserId { get; set; } = "";
+
+        public List<int> RecipeProducts { get; set; } = new List<int>();
+
+        public List<int> RecipeCategories { get; set; } = new List<int>();
+    }
+}
