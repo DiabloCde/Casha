a8caa6c7d0ec6096730068836d96a03178ce2ada
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-12-15 21:24:10 +0200

    IRecipeService specify GetRecipesByFilter method

--- CashaMobile/CashaMobile/CashaMobile/Services/Interfaces/IRecipeService.cs
+++ CashaMobile/CashaMobile/CashaMobile/Services/Interfaces/IRecipeService.cs
@@ -1,4 +1,5 @@
 ﻿using CashaMobile.Models;
+using CashaMobile.Models.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +10,7 @@ namespace CashaMobile.Services.Interfaces
     public interface IRecipeService
     {
         Task<Recipe> GetRecipeByID(int recipeId);
+
+        Task<ICollection<Recipe>> GetRecipesByFilter(RecipeFilter filter);
     }
 }
