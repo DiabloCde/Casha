b311d291351b1546f960cfead61c952da8f1f4ca
Vlad Bocharov <vladislavboc11@gmail.com>
2022-12-15 18:59:10 +0200

    Added fuction on mobile front-end page to get RecipesCard by clicking on item in list of RecipesCard on View

--- CashaMobile/CashaMobile/CashaMobile/Views/RecipesByProductPage.xaml.cs
+++ CashaMobile/CashaMobile/CashaMobile/Views/RecipesByProductPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CashaMobile.ViewModels;
+﻿using CashaMobile.Models;
+using CashaMobile.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,14 @@ namespace CashaMobile.Views
             InitializeComponent();
             ViewModel = new RecipesByProductViewModel();
             BindingContext = ViewModel;
+        }
+
+        private void ViewCell_Tapped(object sender, EventArgs e)
+        {
+            ViewCell viewCell = sender as ViewCell;
+            RecipesCard card = (RecipesCard)viewCell.BindingContext;
+            // You can get RecipesCard to ViewModel by clicking on item in list of RecipesCards
+            // Console.WriteLine(card.RecipeName);
         }
     }
 }
