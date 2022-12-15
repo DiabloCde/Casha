65dc3cbdbf11cd923462887794996524cde04ac9
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-12-16 00:14:25 +0200

    Load recipes on appearing page

--- CashaMobile/CashaMobile/CashaMobile/Views/RecipesByProductPage.xaml.cs
+++ CashaMobile/CashaMobile/CashaMobile/Views/RecipesByProductPage.xaml.cs
@@ -3,6 +3,7 @@ using CashaMobile.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,16 +19,28 @@ namespace CashaMobile.Views
         public RecipesByProductPage(UserProduct userProduct)
         {
             InitializeComponent();
-            ViewModel = new RecipesByProductViewModel(userProduct);
+            ViewModel = Startup.Resolve<RecipesByProductViewModel>();
+            ViewModel.UserProduct = userProduct;
             BindingContext = ViewModel;
         }
 
+        private void Search(object sender, EventArgs e)
+        {
+            var SearchString = searchBar.Text;
+            Console.WriteLine(SearchString);
+        }
         private void ViewCell_Tapped(object sender, EventArgs e)
         {
             ViewCell viewCell = sender as ViewCell;
             RecipesCard card = (RecipesCard)viewCell.BindingContext;
             // You can get RecipesCard to ViewModel by clicking on item in list of RecipesCards
             // Console.WriteLine(card.RecipeName);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ViewModel.LoadRecipes.Execute(null);
         }
     }
 }
